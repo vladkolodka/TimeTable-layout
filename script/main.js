b24850756c6a93e96a47bd01c03a11ce0f004f4d
Vladislav Kolodka <vladkolodka@gmail.com>
2015-12-10 20:41:53 +0200

    Added submenu for localities

--- script/main.js
+++ script/main.js
@@ -1,10 +1,14 @@
 $(function () {
-    $("a[data-region]").click(function (event) {
-        alert( $(event.target).attr("data-region"));
+    $("#main-navigation").find("a[data-region]").click(function (event) {
+        var content = $(".hidden_block_templates .submenu_" + $(event.target).attr("data-region"));
+        $("#menu_title").html(content.attr("data-name"));
+        $(".submenu-content").html(content.html());
+        $("#main-navigation, .submenu").addClass("submenu_opened");
 
         return false;
     });
+    $("#backToMainMenu").click(function () {
+        $("#main-navigation, .submenu").removeClass("submenu_opened");
+        $("#menu_title").html("Области");
+    });
 });
-function getBackButton(){
-
-}
